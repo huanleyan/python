71e1f0db999f57f810c3bd6f0640ab0d06ed3b01
huanleyan <chenghuan08@163.com>
2018-01-23 10:52:35 +0800

    新增联系

---
+++ 按照比例缩小图片并生成新图片.py
@@ -0,0 +1,25 @@
+import os
+from PIL import Image
+ext = ['jpg','jpeg','png']
+files = os.listdir('.')
+
+def process_image(filename, mwidth=200, mheight=400):
+    image = Image.open(filename)
+    w,h = image.size
+    if w<=mwidth and h<=mheight:
+        print(filename,'is OK.')
+        return 
+    if (1.0*w/mwidth) > (1.0*h/mheight):
+        scale = 1.0*w/mwidth
+        new_im = image.resize((int(w/scale), int(h/scale)), Image.ANTIALIAS)
+
+    else:
+        scale = 1.0*h/mheight
+        new_im = image.resize((int(w/scale),int(h/scale)), Image.ANTIALIAS)     
+    new_im.save('new-'+filename)
+    new_im.close()
+
+for file in files:
+    if file.split('.')[-1] in ext:
+        process_image(file)
+
